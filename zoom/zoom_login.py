43e6c4674285fcac9a6883a93a47b9f489630098
Jac Cottam <jac.cottam5@gmail.com>
2022-07-22 18:13:24 -0400

    Updated zoom script

--- zoom/zoom_login.py
+++ zoom/zoom_login.py
@@ -1,7 +1,17 @@
 import webbrowser
 
-# Executing the below will automatically open the meeting in Zoom
+# Open zoom and make sure that it's running 
+# LOG IN in your webbrowser - this will prevent you from logging out
 
-url = 'https://us04web.zoom.us/j/406536344'
-webbrowser.open(url , new = 0)
+# Open the meeting
 
+url_zoom = 'https://us04web.zoom.us/j/406536344'
+webbrowser.open(url_zoom , new = 0)
+
+# Open the script and daily reflection
+
+url_drive = 'https://docs.google.com/document/d/1tVsWegpQ8LR7J20m8sRVA9z-XuKPzDwX7d8yQER-X1c/view'
+url_dr  = 'https://www.aa.org/pages/en_US/daily-reflection'
+
+webbrowser.open(url_drive, new = 0)
+webbrowser.open(url_dr, new = 0)
