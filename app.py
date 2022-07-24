ac18cf09783be2c11d6c083bcc99de8899e8e4e0
Jac Cottam <jac.cottam5@gmail.com>
2022-07-24 00:07:45 -0400

    Access token achieve

--- app.py
+++ app.py
@@ -5,31 +5,36 @@ from flask.json import jsonify
 from credentials import client_id, client_secret
 import os
 
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = "1"
+
 app = Flask(__name__)
+app.debug = True
 app.secret_key = 'jakcjklnsdfn'
 app.config['SESSION_COOKIE_NAME'] = 'Jacs Cookie'
 
 authorization_base_url = 'https://timetreeapp.com/oauth/authorize'
 token_url = 'https://timetreeapp.com/oauth/token'
-redirect_uri = 'http://127.0.0.1:5000/'
+redirect_uri = 'http://127.0.0.1:5000'
 
-@app.route('/authorize')
+@app.route('/auth')
 def demo():
     timetree = OAuth2Session(client_id=client_id, redirect_uri=redirect_uri)
     authorization_url, state = timetree.authorization_url(authorization_base_url)
     session['oauth_state'] = state
-    print('Auth URL is:\t')
     return redirect(authorization_url)
-    print(redirect(authorization_url))
-    return redirect('http://127.0.0.1:5000/accept')
 
-@app.route('/accept')
+@app.route('/')
 def accept(): 
-    return 'URL shoudld be gtg now!'
+    timetree = OAuth2Session(client_id, redirect_uri=redirect_uri, state=session['oauth_state'])
+    token = timetree.fetch_token(token_url, client_secret=client_secret, authorization_response=request.url)
+    session['oauth_token'] = token
+    print('Token has been stored:\t')
+    print(token)
+    return 'Check command prompt for token.'
 
 if __name__ == "__main__":
     # This allows us to use a plain HTTP callback
     os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = "1"
 
     app.secret_key = os.urandom(24)
-    app.run(debug=True)
+    app.run()
