c0b63e90d260a3570d3fdd7ff3fd8197415be1c8
Jac Cottam <jac.cottam5@gmail.com>
2022-07-24 15:08:58 -0400

    Consolidated API requests into one .ipynb file

--- zoom/host_zoom.py
+++ zoom/host_zoom.py
@@ -10,12 +10,12 @@ print('Make sure you are logged into the host account!\t')
 print('Just one second and Ill pull up the meeting, script, and daily reflection for you!')
 
 url_zoom = 'https://us04web.zoom.us/j/406536344'
-webbrowser.open(url_zoom , new = 0)
+webbrowser.open(url_zoom , new = 2)
 
 # Open the script and daily reflection
 
 url_drive = 'https://docs.google.com/document/d/1tVsWegpQ8LR7J20m8sRVA9z-XuKPzDwX7d8yQER-X1c/view'
 url_dr  = 'https://www.aa.org/pages/en_US/daily-reflection'
 
-webbrowser.open(url_drive, new = 0)
-webbrowser.open(url_dr, new = 0)
+webbrowser.open(url_drive, new = 2)
+webbrowser.open(url_dr, new = 2)
