cd39fc8d0b9d66f1bdefde39da63cfc8c5c4c725
Jac Cottam <jac.cottam5@gmail.com>
2022-07-24 10:37:14 -0400

    Updated requests.py

--- requests.py
+++ requests.py
@@ -1,7 +1,8 @@
 import requests 
 import json
-import pandas as pd
 from credentials import headers
+
+# TIMETREE DEV API : https://developers.timetreeapp.com/en/docs/api/oauth-app#list-members
 
 url = 'https://timetreeapis.com/calendars/SRx7BmTyN7Hw/members'
 
@@ -17,10 +18,3 @@ print(r.text)
 # with open('members.json', 'w') as f:
 #     json.dump(r.text, f, indent = 4)
 #     f.close()
-
-# r = requests.get('https://timetreeapp.com/oauth/calendars', headers=headers)
-# print(r.url)
-
-# api = TimeTreeApi('API_ACCESS_TOKEN')
-# calendar = api.get_calendar('CALENDAR_ID')
-# print(calendar.data.attributes.name) # calendar name
