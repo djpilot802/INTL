e075095e3567d27dde92651449f1cb5703de3299
djpilot802 <108034915+djpilot802@users.noreply.github.com>
2022-07-24 01:32:16 -0400

    Delete token.py

--- token.py
+++
@@ -1,7 +0,0 @@
-{'access_token': '1rUYDGFp18SwqQHrXJsJ5gsKor0xdvRjTw7QkNh3o3w', 
-'token_type': 'Bearer', 
-'scope': ['read:user', 'read:calendar', 'read:member', 'read:event', 'write:event', 'write:comment'], 
-'created_at': 1658583009}
-
-url = 'https://timetreeapp.com/oauth/calendars'
-
