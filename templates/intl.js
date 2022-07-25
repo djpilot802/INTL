3ad8838dc85af17f55e5d724022d20d7912a3a36
Jac Cottam <jac.cottam5@gmail.com>
2022-07-24 22:56:23 -0400

    Updating html with some js.

---
+++ templates/intl.js
@@ -0,0 +1,14 @@
+function NewTab() {
+    window.open(
+    "https://us04web.zoom.us/j/406536344", "_blank");
+}
+
+function NewTab() {
+    window.open(
+    "https://us04web.zoom.us/j/406536344", "_blank");
+}
+
+function NewTab() {
+    window.open(
+    "https://us04web.zoom.us/j/406536344", "_blank");
+}
