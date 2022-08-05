cffb694c65bc4eb96fd490bde4abf8dd016c1d86
Jac Cottam <jac.cottam5@gmail.com>
2022-08-05 01:38:09 -0400

    Updated zoom opening function

--- zoom/participant_zoom.py
+++ zoom/participant_zoom.py
@@ -9,22 +9,20 @@ import numpy as np
 print('BOOTING UP UP UP....')
 print('Have you logged into zoom on your web browser? If not - just go to:\t')
 print('https://zoom.us/\t')
-print('Now type anything to continue')
+print('Now save the password to your clipboard and click any key to continue..\t')
+print('\t')
+print('easyDoesit')
 
 input()
 
-url_zoom = 'https://us04web.zoom.us/j/406536344?pwd=easyDoesit'
-webbrowser.open(url_zoom , new = 0)
-
-print('Paste below passcode in zoom:\t')
-print('\t')
-print('easyDoesit')
-print('\t')
 print('Want to open the script and daily reflections? (TYPE Y or N)\t')
 print('\t')
 response = input()
 url_drive = 'https://docs.google.com/document/d/1tVsWegpQ8LR7J20m8sRVA9z-XuKPzDwX7d8yQER-X1c/view'
 url_dr  = 'https://www.aa.org/pages/en_US/daily-reflection'
+
+url_zoom = 'https://us04web.zoom.us/j/406536344?pwd=easyDoesit'
+webbrowser.open(url_zoom , new = 0)
 
 if response == 'Y': 
     webbrowser.open(url_drive, new = 0)
